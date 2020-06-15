74bf557fa667a2a2ca3825bd8dfb8f10d507e39d
Maheshkumar Palled <maheshmechx@gmail.com>
2020-06-14 20:48:02 -0700

    comment to revert?

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Main from './Components/Main';
 import { NavigationContainer } from '@react-navigation/native'
 
+//test commit to revert
 export default function App() {
   return (
     <NavigationContainer>
